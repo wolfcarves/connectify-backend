4f05b9f7b4214eaa7b298421c68056059fe4ca15
Rodel <rodel.crisosto7@gmail.com>
2024-11-08 16:30:59 +0800

    chore: adjust db connection configuration 02

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,14 +4,6 @@ import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { env } from '@/config/env';
 
-const globalDb = globalThis as unknown as {
-	conn: postgres.Sql | undefined;
-};
-
-const conn = globalDb?.conn ?? postgres(env?.databaseUri as string);
-
-if (process.env.NODE_ENV !== 'production') {
-	globalDb.conn = conn;
-}
+const conn = postgres(env?.databaseUri as string);
 
 export const db = drizzle(conn);
